5daa7208000b54a46eda408116435bc63c26e3b5
Natnipa S <s.natnipa@gmail.com>
2020-10-13 14:13:58 +0700

    Revert "add path"
    This reverts commit b780ab6d54bfcff3d7aefdb83a99460693188baa.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,20 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // we only want single quotes
+    'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'always'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error'],
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
