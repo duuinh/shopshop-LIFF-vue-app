9a557e3e3494dc84b2f73b51cb870ba5e7968222
Natnipa S <s.natnipa@gmail.com>
2020-10-13 14:40:21 +0700

    Revert "fix path"
    This reverts commit 249ef1cd49b495c5e82d853ed4de287c587dabeb.

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-// server.js
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express()
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
-
-app.use(staticFileMiddleware)
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
-app.use(staticFileMiddleware)
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
-})
-
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port
-  console.log('App now running on port', port)
-})
+var port = process.env.PORT || 5005
+app.listen(port)
+console.log('server started ' + port)
