76548df41c49b17f0c17d062156b8d788921e1bc
Natnipa S <s.natnipa@gmail.com>
2020-10-13 18:38:59 +0700

    fix order

--- src/views/AddToCart.vue
+++ src/views/AddToCart.vue
@@ -138,8 +138,7 @@ export default {
       this.isAdded = true;
       // let url = 'http://192.168.43.201:8000/api/orders/place-order';
       let url = 'https://shopvisor.azurewebsites.net/api/orders/place-order';
-      this.sendMessage('เพิ่ม '+this.productId+' ในตะกร้าสินค้า');
-      await axios.get(url, {
+      axios.get(url, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Accept': 'application/json'
@@ -152,7 +151,8 @@ export default {
           qty: this.qty,
         }
       });
-      // await this.sleep(2000);
+      await this.sleep(1500);
+      await this.sendMessage('เพิ่ม '+this.productId+' ในตะกร้าสินค้า');
       this.closeWindow();
     },
     sleep(ms) {
@@ -165,7 +165,7 @@ export default {
       this.sendMessage('ไม่เอาดีกว่า');
       this.closeWindow();
     },
-    sendMessage(message) {
+    async sendMessage(message) {
       this.$liff.sendMessages([{
         type: 'text',
         text: message
